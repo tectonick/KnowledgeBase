66ae913678397179a85669e31580cfdb413e8fd9
Nick Kiselev <korn9509@gmail.com>
2020-05-17 18:52:48 +0300

    ReviewCounter colors

--- KnowledgeBase/ViewComponents/ReviewCounter.cs
+++ KnowledgeBase/ViewComponents/ReviewCounter.cs
@@ -1,7 +1,9 @@
 ﻿using KnowledgeBase.Models;
 using KnowledgeBase.Repositories;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,12 +21,31 @@ namespace KnowledgeBase.ViewComponents
             _topicRepository = topicRepository;
             _userManager = userManager;
         }
-        public string Invoke()
+        public IViewComponentResult Invoke()
         {
             
             var allTopics = _topicRepository.GetAllForUser(_userManager.GetUserId(HttpContext.User));
-            int todayTopicsCount = allTopics.Where(t => (t.NextRepeat>DateTime.MinValue) &&(t.NextRepeat.Date <= DateTime.Now.Date)).Count();
-            return todayTopicsCount.ToString();
+            var todayTopics= allTopics.Where(t => (t.NextRepeat > DateTime.MinValue) && (t.NextRepeat.Date <= DateTime.Now.Date)).ToList();
+            int todayTopicsCount = todayTopics.Count();
+            int obsoleteTopicsCount = todayTopics.Where(t => t.NextRepeat < DateTime.Now).Count();
+            if (todayTopicsCount>0)
+            {
+                string badgeClass = (obsoleteTopicsCount > 0) ? "badge-danger" : "badge-primary";
+                return new HtmlContentViewComponentResult(
+                new HtmlString($"<span class=\"badge badge-pill {badgeClass}\">{todayTopicsCount.ToString()}</span>")
+                );
+            }
+            else
+            {
+                return new HtmlContentViewComponentResult(
+                new HtmlString($"")
+                );
+                //return new HtmlContentViewComponentResult(
+                //new HtmlString($"<span class=\"badge badge-pill badge-success\"><i class=\"fas fa-check\"></i></span>")
+                //);
+            }
+
+            
         }
     }
 }
